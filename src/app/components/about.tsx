318271ee47c21ff6a283884b9ad09945ab86be71
courage okpa <ceocoetta2009@gmail.com>
2023-08-28 13:34:19 -0700

    added a new component

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -6,25 +6,25 @@ const Card = () => {
      <>
        <div className="w-full md:h-[570px] bg-indigo-800">
         <p className="text-center text-white text-xl pt-10 font-Poppins">ONLINE TRAVEL AGENCY</p>
-        <h2 className="text-center text-white text-3xl pt-3 pb-5 font-[poppins]">We Help Travelers</h2>
+        <h2 className="text-center text-white text-3xl pt-3 pb-5 font-Poppins">We Help Travelers</h2>
     
         <div className="container md:h-[410px] md:ml-8 md:grid md:grid-cols-4 md:gap-4 justify-items-center grid grid-rows-4 gap-y-5">
             <div className="w-[300px] md:h-[400px] bg-white rounded-md">
-                <h1 className="text-center pt-4 text-indigo-800 text-2xl font-Poppins">TRAVEL</h1>
+                <h1 className="text-center pt-4 text-indigo-900 text-2xl font-Poppins">TRAVEL</h1>
                 <p className="text-center p-5 text-indigo-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dolorem laborum enim perspiciatis aperiam amet quo sint voluptatem molestiae eaque similique veritatis eos repellendus voluptas vero laboriosam aspernatur architecto suscipit dicta officiis beatae. Autem ipsum numquam aliquid repudiandae deleniti. Id.</p>
             </div>
             <div className="w-[300px] md:h-[400px] bg-white rounded-md">
-                <h1 className="text-center pt-4 text-indigo-800 text-2xl font-[poppins]">BENEFITS</h1>
+                <h1 className="text-center pt-4 text-indigo-900 text-2xl font-Poppins">BENEFITS</h1>
                 <p className="text-center p-5 text-indigo-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dolorem laborum enim perspiciatis aperiam amet quo sint voluptatem molestiae eaque similique veritatis eos repellendus voluptas vero laboriosam aspernatur architecto suscipit dicta officiis beatae. Autem ipsum numquam aliquid repudiandae deleniti. Id.</p>
 
             </div>
             <div className="w-[300px] h-[400px] bg-white rounded-md">
-                <h1 className="text-center pt-4 text-indigo-800 text-2xl font-[poppins]">ABOUT</h1>
+                <h1 className="text-center pt-4 text-indigo-900 text-2xl font-Poppins">ABOUT</h1>
                 <p className="text-center p-5 text-indigo-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dolorem laborum enim perspiciatis aperiam amet quo sint voluptatem molestiae eaque similique veritatis eos repellendus voluptas vero laboriosam aspernatur architecto suscipit dicta officiis beatae. Autem ipsum numquam aliquid repudiandae deleniti. Id.</p>
 
             </div>
             <div className="w-[300px] h-[400px] bg-white rounded-md">
-                <h1 className="text-center pt-4 text-indigo-800 text-2xl font-[poppins]">AWARDS</h1>
+                <h1 className="text-center pt-4 text-indigo-900 text-2xl font-Poppins">AWARDS</h1>
                 <p className="text-center p-5 text-indigo-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dolorem laborum enim perspiciatis aperiam amet quo sint voluptatem molestiae eaque similique veritatis eos repellendus voluptas vero laboriosam aspernatur architecto suscipit dicta officiis beatae. Autem ipsum numquam aliquid repudiandae deleniti. Id.</p>
             </div>
         </div>
