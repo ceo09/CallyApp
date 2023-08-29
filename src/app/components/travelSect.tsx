216464f5f8fc5f7214aaf15d2e3e4d94c95c869f
courage okpa <ceocoetta2009@gmail.com>
2023-08-29 03:12:35 -0700

    worked on some compoents

---
+++ src/app/components/travelSect.tsx
@@ -0,0 +1,25 @@
+const TravelCard = () => {
+    return (
+    <div className="container justify-items-center md:grid md:grid-cols-3 md:grid-rows-2 gap-4">
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+        <div className="md:w-[400px] md:h-[400px] bg-slate-100 rounded-md">
+
+        </div>
+    </div>
+    )
+}
+export default TravelCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Card from "./components/about"
+import TravelCard from "./components/travelSect"
 
 export default function Home() {
   return (
+  <div>
     <Card />
-      )
+    <TravelCard />
+  </div>
+    )
 }
